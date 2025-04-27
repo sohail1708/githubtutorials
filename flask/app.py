9b13de1d93035c165317e350f91279e519263b58
Sohail Shaik <sohailshaik@Sohails-MacBook-Air.local>
2025-04-26 21:48:08 -0500

    Initial commit with project files and .gitignore

---
+++ flask/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return render_template("welcome.html")
+
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template("about.html")
+
+@app.route("/form", methods=["GET", "POST"])
+def form_contact():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+        return f"Thank you {name} for your message: {message}. We will contact you at {email}."
+    return render_template("form.html")
+
+@app.route("/success/<int:score>")
+def success(score):
+    if score >=50:
+        result = "Congratulations! You passed the exam."
+    else:
+        result = "Sorry, you failed the exam."
+    return render_template("/success.html", result=result)
+
+@app.route("/profile/<username>")
+def profile(username):
+    return render_template("profile.html", username=username)
+
+if __name__ == "__main__":
+    app.run(debug=True)
